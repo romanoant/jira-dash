4f573755960b39e41ed1a5962ac5483c28e640d8
Ivan Loire <iloire@atlassian.com>
2013-05-30 11:10:57 +1000

    Shipit modifications. Remove console log on countdown

--- jobs/countdown/countdown.js
+++ jobs/countdown/countdown.js
@@ -1,9 +1,4 @@
 module.exports = function(config, dependencies, job_callback) {
-  
-  console.log('----------------------');
-  console.log(dependencies.moment(config.dueDate).fromNow());
-  console.log('----------------------');
-  
   job_callback(null, {
     when: dependencies.moment(config.dueDate).fromNow(), 
     milestone: config.milestone
