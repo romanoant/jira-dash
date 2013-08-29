e45a638c5f17fda97b9949967494587e9be084d8
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2013-08-29 16:36:20 +0200

    added widget for displaying Crucible reviews for a team

---
+++ widgets/reviewscount/reviewscount.js
@@ -0,0 +1,34 @@
+widget = {
+    //runs when we receive data from the job
+    onData: function(el, data) {
+        if (data.title) {
+            $('h2', el).text(data.title);
+        }
+
+		$('.content', el).empty();
+
+        data.reviews.sort(function(a, b) {
+        	return a.username > b.username;
+        });
+
+		data.reviews.forEach(function(user) {
+			var $user = $("<div class='user'></div>");
+			$user.text(user.username);
+
+			$user.append("<div class='result'>" + user.openReviews + "</div");
+
+          	$('.content', el).append($user);
+		});
+
+		// Choose maximum font size with which all content will still fit
+        var fontSize = 60;
+        var $content = $('.content', el);
+        var maxHeight = $(el).parent().height() - $('h2', el).outerHeight(true);
+        var textHeight;
+        do {
+          $content.css('font-size', fontSize);
+          textHeight = $content.outerHeight(true);
+          fontSize = fontSize - 1;
+        } while (textHeight > maxHeight && fontSize > 3);
+    }
+};
