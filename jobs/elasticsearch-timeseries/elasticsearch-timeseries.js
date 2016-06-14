80b74bb9d4692bd6993a6343f6327c8a9e6fc625
Rigas Grigoropoulos <rgrigoropoulos@atlassian.com>
2016-06-14 15:28:05 +1000

    PUR-8708 - request exact index names when an index with wildcard is configured

--- jobs/elasticsearch-timeseries/elasticsearch-timeseries.js
+++ jobs/elasticsearch-timeseries/elasticsearch-timeseries.js
@@ -71,38 +71,78 @@ module.exports = {
       });
     }
 
+    function authHeader() {
+      return "Basic " + new Buffer(config.globalAuth[config.authName].username + ":" +
+              config.globalAuth[config.authName].password).toString("base64");
+    }
+
+    function getIndices(query, cb) {
+      // If configured index does not contain wildcard, return as configured
+      if (!query.index.endsWith('*')) {
+        return cb(null, {indices: [query.index]});
+      }
+
+      var options = {
+        method: 'POST',
+        url: 'https://' + config.host + ':' + config.port + '/' + query.index + '/_field_stats?level=indices',
+        json: true,
+        body: {
+          "fields": ["@timestamp"]
+        },
+        headers: {
+          "authorization": authHeader()
+        }
+      };
+      request(options, function (err, httpResponse, body) {
+        if (err || httpResponse.statusCode !== 200) {
+          return cb(err ? err : 'status code: ' + httpResponse.statusCode);
+        }
+        cb(null, {indices: _.keys(body.indices)});
+      });
+    }
+
     function doQuery(serie, cb) {
       if (!serie.index || !serie.query) {
         return cb('invalid config parameters for serie');
       }
-      var queryUrl = url.format({
-        protocol: 'https',
-        host: config.host + ':'  + (config.port || 9200),
-        pathname: serie.index + '/_search'
-      });
 
-      var options = {
-        method: 'POST',
-        url : queryUrl,
-        json: true,
-        body: queryBuilder.buildHistogramQuery(serie.query, config.groupBy, config.queryOptions),
-        headers: {
-          "authorization": "Basic " + new Buffer(config.globalAuth[config.authName].username + ":" +
-              config.globalAuth[config.authName].password).toString("base64")
+      getIndices(serie, function (error, data) {
+        if (error) {
+          return cb(error);
         }
-      };
 
-      request(options, function (err, httpResponse, body) {
-        if (err || httpResponse.statusCode !== 200) {
-          var msg = err ? err : 'status code: ' + httpResponse.statusCode + ' body: ' + JSON.stringify(body);
-          return cb(msg);
+        if (_.isEmpty(data.indices)) {
+          return cb(null, {});
         }
-        if (!body.aggregations || !body.aggregations.results) {
-          return cb('Unexpected data received');
-        }
-        cb(null, {
-          data: massageDataset(body.aggregations.results.buckets),
-          options: _.omit(serie, ['query', 'index'])
+
+        var queryUrl = url.format({
+          protocol: 'https',
+          host: config.host + ':'  + (config.port || 9200),
+          pathname: data.indices.join() + '/_search'
+        });
+
+        var options = {
+          method: 'POST',
+          url : queryUrl,
+          json: true,
+          body: queryBuilder.buildHistogramQuery(serie.query, config.groupBy, config.queryOptions),
+          headers: {
+            "authorization": authHeader()
+          }
+        };
+
+        request(options, function (err, httpResponse, body) {
+          if (err || httpResponse.statusCode !== 200) {
+            var msg = err ? err : 'status code: ' + httpResponse.statusCode + ' body: ' + JSON.stringify(body);
+            return cb(msg);
+          }
+          if (!body.aggregations || !body.aggregations.results) {
+            return cb('Unexpected data received');
+          }
+          cb(null, {
+            data: massageDataset(body.aggregations.results.buckets),
+            options: _.omit(serie, ['query', 'index'])
+          });
         });
       });
     }
