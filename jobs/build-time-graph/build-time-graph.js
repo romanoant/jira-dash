da0fee5a87f91e3885822640688bd03a46e778ee
Adam Hynes <ahynes@atlassian.com>
2016-07-14 16:53:21 +0200

    Added build time graph job

---
+++ jobs/build-time-graph/build-time-graph.js
@@ -0,0 +1,44 @@
+/*
+
+ Build time graph job.
+
+   "build-time-graph-UI" : {
+   "bamboo_server" : "https://collaboration-bamboo.internal.atlassian.com",
+   "retryOnErrorTimes" : 3,
+   "interval" : 120000,
+   "widgetTitle" : "MASTER CI BUILD TIME"
+ }
+
+ */
+
+var Bamboo = require('./lib/bamboo.js'),
+    cache = require('memory-cache'),
+    cheerio = require('cheerio');
+
+module.exports = function(config, dependencies, job_callback) {
+    // fallback to for configuration compatibility
+    var authName = config.authName || 'cbac';
+
+    if (!config.globalAuth || !config.globalAuth[authName] ||
+        !config.globalAuth[authName].username || !config.globalAuth[authName].password){
+        return job_callback('No Bamboo credentials found in buildoverview job for authName \'' + authName + '\'. Please check global authentication file');
+    }
+
+    if (!config.bamboo_server){
+        return job_callback("No bamboo server configured");
+    }
+
+    var logger = dependencies.logger;
+
+    var username = config.globalAuth[authName].username;
+    var password = config.globalAuth[authName].password;
+    var bamboo = new Bamboo(config.bamboo_server, username, password, dependencies.request, cache, cheerio);
+
+    bamboo.getBuildTimeChartUrl(planKey, function(err, graphUrl) {
+        if (err || !graphUrl){
+            job_callback(err)
+        }
+
+        job_callback(null, graphUrl)
+    })
+}
