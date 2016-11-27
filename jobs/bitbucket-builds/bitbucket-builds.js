74ec1d5c75dab85cda1c1b579e6538e43ce69531
Ben Gummer <bgummer@atlassian.com>
2016-11-28 08:25:27 +1100

    chore: fix indentation

--- jobs/bitbucket-builds/bitbucket-builds.js
+++ jobs/bitbucket-builds/bitbucket-builds.js
@@ -18,32 +18,32 @@ var getBranchBuildStatus = require('./helpers/getBranchBuildStatus');
 
 module.exports = {
 
-    /**
-    * Executed every interval
-    * @param config
-    * @param dependencies
-    * @param jobCallback
-    */
-    onRun: function (config, dependencies, jobCallback) {
-        var jobWorker = this;
+  /**
+  * Executed every interval
+  * @param config
+  * @param dependencies
+  * @param jobCallback
+  */
+  onRun: function (config, dependencies, jobCallback) {
+    var jobWorker = this;
 
-        // Ensure username + password supplied
-        var credentialsKey = config.credentials;
-        if (!config.globalAuth || !config.globalAuth[credentialsKey] || !config.globalAuth[credentialsKey].username || !config.globalAuth[credentialsKey].password) {
-            return jobCallback('No Bitbucket credentials found for pipelines-builds job. Please check global authentication file.');
-        }
+    // Ensure username + password supplied
+    var credentialsKey = config.credentials;
+    if (!config.globalAuth || !config.globalAuth[credentialsKey] || !config.globalAuth[credentialsKey].username || !config.globalAuth[credentialsKey].password) {
+      return jobCallback('No Bitbucket credentials found for pipelines-builds job. Please check global authentication file.');
+    }
 
-        getBranchList(config).then(function(branchInfos) {
-            // Load statuses for each branch and push back to the client live
-            branchInfos.forEach(function(branchInfo) {
-                getBranchBuildStatus(jobWorker, branchInfos, config, branchInfo.branchName);
-            });
+    getBranchList(config).then(function(branchInfos) {
+      // Load statuses for each branch and push back to the client live
+      branchInfos.forEach(function(branchInfo) {
+        getBranchBuildStatus(jobWorker, branchInfos, config, branchInfo.branchName);
+      });
 
-            // Send the branch list back to the client (without statuses)
-            jobCallback(null, {
-                title: config.title,
-                branchStatuses: branchInfos,
-            });
-        });
-    },
+      // Send the branch list back to the client (without statuses)
+      jobCallback(null, {
+        title: config.title,
+        branchStatuses: branchInfos,
+      });
+    });
+  },
 };
